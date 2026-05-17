eaf51489450b748d1f85aa67063034fd101f274c
Mansi <mansidakhalee@gmail.com>
2026-05-17 19:40:40 +0530

    Added CORS middleware to call backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,9 +12,19 @@ from app.db.database import Base, engine
 
 from app.models.user import User
 
+from fastapi.middleware.cors import CORSMiddleware
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="OpsPilot AI Backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Register Routers
 app.include_router(chat_router, prefix="/ai", tags=["AI Chat"])
