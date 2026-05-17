b0e43f20d7c05c8c7c90c0ec05ac687efb1f14ab
Mansi <mansidakhalee@gmail.com>
2026-05-17 16:12:24 +0530

    refactor database configuration using environment variables

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,8 +1,13 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://opspilot:opspilot@postgres:5432/opspilot_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
